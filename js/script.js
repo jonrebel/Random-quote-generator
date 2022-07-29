d4b273536661b9256fa30978f09e76418cd551ff
jonrebel <110244002+jonrebel@users.noreply.github.com>
2022-07-29 13:40:09 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,18 +10,39 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+const quotes = [
+  {quote:'The journey of a thousand miles begins with one step.', source:'Lao Tzu'},
+  {quote:'That which does not kill us makes us stronger.', source:'Friedrich Nietzsche'},
+  {quote:'Life is what happens when youre busy making other plans.', source:'John Lennon'},
+  {quote:'When the going gets tough, the tough get going."', source:'Joe Kennedy'},
+  {quote:'You must be the change you wish to see in the world.', source:'Mahatma Gandhi'},
+  {quote:'You only live once, but if you do it right, once is enough.', source:'Mae West'},
+  {quote:'Tough times never last but tough people do.', source:'Robert H. Schuller'}
+]
 
 
 
 /***
  * `getRandomQuote` function
 ***/
-
+function getRandomQuote(){
+  for(let i = 0; i < quotes.length;i++){
+    let pointer = Math.floor(Math.random()*7);
+    let quote = quotes[pointer];
+    return quote;
+  }
+}
 
 
 /***
  * `printQuote` function
 ***/
+function printQuote(){
+  let ranQ = getRandomQuote();
+  document.getElementById('quote-box').innerHTML =`
+  <p class="quote">${ranQ.quote}</p>
+  <p class="source">${ranQ.source}</p>`
+}
 
 
 
